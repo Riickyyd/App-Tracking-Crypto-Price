4cf413be5a084448a09ff43924a0ce39b431d2d5
Ricky Nguyen <110729777+Riickyyd@users.noreply.github.com>
2022-10-09 14:15:52 +0700

    Update Tracking_Crypto_App.py

--- Tracking_Crypto_App.py
+++ Tracking_Crypto_App.py
@@ -4,6 +4,12 @@ import datetime
 from datetime import datetime
 import pytz
 
+[theme]
+primaryColor="#6eb52f"
+backgroundColor="#f0f0f5"
+secondaryBackgroundColor="#e0e0ef"
+textColor="#262730"
+font="sans serif"
 
 st.markdown('# **Tracking Cryptocurrency Price**')
 st.markdown('Powered by Ricky')
