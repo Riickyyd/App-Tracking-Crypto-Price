3c7740e34e160feca24fe81788d2a6f74a1cc4e7
Ricky Nguyen <110729777+Riickyyd@users.noreply.github.com>
2022-10-09 14:48:43 +0700

    Update Tracking_Crypto_App.py

--- Tracking_Crypto_App.py
+++ Tracking_Crypto_App.py
@@ -83,7 +83,7 @@ st.write('Update at: ', t)
 
 #create a linechart
 chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
+    np.random.randn(10, 3),
     columns=['a', 'b', 'c'])
 
 st.area_chart(chart_data)
