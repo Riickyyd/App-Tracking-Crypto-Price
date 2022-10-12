a37c1bb5c3870e715f75bee58168d1c706b137f9
Ricky Nguyen <110729777+Riickyyd@users.noreply.github.com>
2022-10-12 08:15:26 +0700

    Update Tracking_Crypto_App.py

--- Tracking_Crypto_App.py
+++ Tracking_Crypto_App.py
@@ -94,10 +94,10 @@ st.header ('All information')
 st.dataframe(df)
 
 #BTC CHART
-Btch = col1_df.priceChangePercent, 
+Btch = (col1_df.priceChangePercent, 
        col2_df.priceChangePercent, 
        col4_df.priceChangePercent, 
-       col5_df.priceChangePercent
+       col5_df.priceChangePercent)
 st.bar_chart(Btch)
 
 #change
