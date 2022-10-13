44f58393dc7bcec69008b33f2953d3e27d536cd0
Ricky Nguyen <110729777+Riickyyd@users.noreply.github.com>
2022-10-13 21:07:21 +0700

    Update Tracking_Crypto_App.py

--- Tracking_Crypto_App.py
+++ Tracking_Crypto_App.py
@@ -103,7 +103,7 @@ st.balloons()
 st.header ('BTC $')
 image2 = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1.png'))
 st.image(image2)
-st.dataframe(df.get("11"))
+st.dataframe(df.get("symbol"))
 
 def info():
     api_link = "https://min-api.cryptocompare.com/data/exchange/histoday?tsym=USD&limit=10"
