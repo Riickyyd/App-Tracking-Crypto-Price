7b37069d5977279f7749b46a75aa6e028d424191
Ricky Nguyen <110729777+Riickyyd@users.noreply.github.com>
2022-10-13 21:04:00 +0700

    Update Tracking_Crypto_App.py

--- Tracking_Crypto_App.py
+++ Tracking_Crypto_App.py
@@ -103,12 +103,12 @@ st.balloons()
 st.header ('BTC $')
 image2 = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1.png'))
 st.image(image2)
-st.bar_chart( col1_df.priceChangePercent,
-              col2_df.priceChangePercent,
-              col3_df.priceChangePercent,
-              col4_df.priceChangePercent,
-              col5_df.priceChangePercent,
-              col6_df.priceChangePercent)
+st.bar_chart( col1_percent,
+              col2_percent,
+              col3_percent,
+              col4_percent,
+              col5_percent,
+              col6_percent)
 
 def info():
     api_link = "https://min-api.cryptocompare.com/data/exchange/histoday?tsym=USD&limit=10"
