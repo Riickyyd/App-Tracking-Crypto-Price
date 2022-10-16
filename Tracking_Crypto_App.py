739586cce44f2b88b0531b272210322a054f4401
Ricky Nguyen <110729777+Riickyyd@users.noreply.github.com>
2022-10-16 14:08:23 +0700

    Update Tracking_Crypto_App.py

--- Tracking_Crypto_App.py
+++ Tracking_Crypto_App.py
@@ -112,7 +112,7 @@ def info():
     dic = req.json()
     
     VND_value =  (dic["RAW"])
-    st.markdown(VND_value)
+    st.text(VND_value)
 #     VND_value =  (dic["VND"])
 #     VND_formatted_value = "{:,.3f}".format(VND_value)
 #     st.write('Price to VNĐ: ', VND_formatted_value + "đ")
