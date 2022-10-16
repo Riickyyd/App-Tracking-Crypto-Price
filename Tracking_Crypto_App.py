d1fd2d54fd190146e288522b099be2db7ec073a7
Ricky Nguyen <110729777+Riickyyd@users.noreply.github.com>
2022-10-16 10:11:30 +0700

    Update Tracking_Crypto_App.py

--- Tracking_Crypto_App.py
+++ Tracking_Crypto_App.py
@@ -112,7 +112,7 @@ def info():
 
     VND_value =  (dic["VND"])
     VND_formatted_value = "{:,.3f}".format(VND_value)
-    st.write('Update at: ', VND_formatted_value)
+    st.write('Price to VNĐ: ', VND_formatted_value + "đ")
 
 
 info ()
