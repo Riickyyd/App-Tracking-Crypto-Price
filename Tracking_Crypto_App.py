975691658cf7a976c9ae3dee96befcd3415523d1
Ricky Nguyen <110729777+Riickyyd@users.noreply.github.com>
2022-10-18 07:17:29 +0700

    Update Tracking_Crypto_App.py

--- Tracking_Crypto_App.py
+++ Tracking_Crypto_App.py
@@ -107,7 +107,7 @@ st.image(image2)
 @st.experimental_memo
 def info():
 #     api_link = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=EUR,VND"
-    api_link = str("https://min-api.cryptocompare.com/data/top/totaltoptiervolfull?limit=10&tsym=USD")
+    api_link = "https://min-api.cryptocompare.com/data/top/volumes?tsym=BTC"
     req = requests.get(api_link)
     list = req.json()
     
